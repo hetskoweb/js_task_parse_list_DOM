33537d48eea8b309e2d9bc82a199466c301f77a2
Illia <hetsko.ilya@gmail.com>
2025-03-15 02:01:22 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+const employeesList = document.querySelectorAll('.list li');
+
+function sortList(list, salaryKey = 'salary') {
+  return list.sort((a, b) => {
+    const salaryA = parseFloat(a[salaryKey].replace(/[^0-9.-]+/g, ''));
+    const salaryB = parseFloat(b[salaryKey].replace(/[^0-9.-]+/g, ''));
+
+    return salaryB - salaryA;
+  });
+}
+
+function getEmployees(list) {
+  return Array.from(list).map((person) => {
+    return {
+      element: person,
+      position: person.getAttribute('data-position'),
+      salary: person.getAttribute('data-salary'),
+      age: person.getAttribute('data-age'),
+    };
+  });
+}
+
+const employees = getEmployees(employeesList);
+const sortedEmployees = sortList(employees);
+
+const listContainer = document.querySelector('.list');
+
+listContainer.innerHTML = '';
+
+sortedEmployees.forEach((employee) => {
+  listContainer.appendChild(employee.element);
+});
